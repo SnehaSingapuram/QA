447ac95d1573d5d7d522f7732c6321ce3abe4953
SnehaSingapuram <sneha.bimagowni@gmail.com>
2017-10-02 11:15:41 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+
+
+namespace Selenium_FirstProjec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver webDriver = new InternetExplorerDriver();
+            webDriver.Navigate().GoToUrl("www.google.com");
+
+
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace BasicTest
+{
+    
+      [TestClass]
+      public class Setup
+      {
+        [TestMethod]
+        public void SetupDllTest()
+        {
+
+
+            IWebDriver driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("www.google.com");
+        }
+
+      }
+        
+    
+}
