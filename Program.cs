ae0c2d969c2e416fa9648430bf45f9eb20b1a2ad
SnehaSingapuram <sneha.bimagowni@gmail.com>
2017-10-02 11:16:45 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,20 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using OpenQA.Selenium;
-using OpenQA.Selenium.IE;
-
-
-namespace Selenium_FirstProjec
+namespace ConsoleApp2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            IWebDriver webDriver = new InternetExplorerDriver();
-            webDriver.Navigate().GoToUrl("www.google.com");
+            int a = 100;
+            double b = 200.1, c;
+
+            c = a + b;
+
+            Console.WriteLine(c);
+
 
 
         }
+
+
     }
 }
